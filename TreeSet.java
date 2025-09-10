417ddadcefd53ede2fe2581d5f1eed607466b403
IsakFelder <IsakFelder@gmail.com>
2025-09-10 21:57:14 +0200

    commit

--- TreeSet.java
+++ TreeSet.java
@@ -92,7 +92,7 @@ public class TreeSet {
             }
             else {
                 iter = iter.left;
-                //hvis iter er root
+                //hvis iter er root, hele treet må flyttes og den høyre under root blir ny root
                 if (iter.parent == null) {
                     
                 }
