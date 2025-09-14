acb48dd51e982cb80f6246517b1db75c8475d20b
OlavWaage <olav.waage@altiboxmail.no>
2025-09-14 06:11:39 +0200

    commit

--- AVL_tree.java
+++ AVL_tree.java
@@ -5,10 +5,127 @@ public class AVL_tree {
     AVLnode root = null;
 
     public AVL_tree(){
+        
+    }
+
+    // insert kallet som kan kalle balanser etter insertRec
+    public void insert(int value) {
+        root = insertRec(root, value);
+        
+    }
+
+    // Rekursiv insert fra roten så vi slipper parent i rotate, mere pekere og flytte mere kode da.
+    private AVLnode insertRec(AVLnode node, int value) {
+        if (node == null) {
+            counter++;
+            return new AVLnode(value); // ny node som blad 
+        }
+
+        // rekursiv obdatering av node dybde
+        if (value < node.data) {
+            node.left = insertRec(node.left, value);
+        } else if (value > node.data) {
+            node.right = insertRec(node.right, value);
+        } else {
+            return node; // duplikat
+        }
+
+        update_dybde(node);
+        //balanser(node); //balanse ved rekursiv veldig lett
+        return node;
+    }
+
+    public void remove(int value){
+        root = removeRec(root, value);
+        
 
     }
 
-    
+    public AVLnode removeRec(AVLnode node, int value){
+
+        if (node == null) return null;
+
+        if (node.data < value){
+            node.left = removeRec(node.left, value );
+        }
+        else if (node.data > value){
+            node.right = removeRec(node.right, value);
+
+        }else{
+
+            if (node.right == null){
+                counter--;
+                node = node.left;
+            }
+
+            else if(node.left == null){
+                counter--;
+                node = node.right;
+            }else{
+                AVLnode midlertidig = finnlav(node);
+                node.data = midlertidig.data;
+                removeRec(midlertidig, midlertidig.data); 
+            }
+        
+        }
+
+        update_dybde(node);
+        //return balanser(node); //balanse ved rekursiv veldig lett for ikke parent pekere
+        return node;// trengs ikke mener jeg
+        
+    }
+
+    //finner den som skal ta over for i remove så man slipper re skrive kode
+    public AVLnode finnlav(AVLnode node){
+        AVLnode min_H = node;
+
+        if (min_H.right != null){
+            min_H = min_H.right;
+        }
+
+        while(min_H.left != null){
+            min_H = min_H.left;
+        }
+        
+        return min_H;
+
+    }
+
+    // setter dybden til noeden hvis de under har riktig verdi 
+    public void update_dybde(AVLnode node){
+
+        node.dybde = 1 + Math.max(dybde_beskyttet(node.left), dybde_beskyttet(node.right));
+    }
+
+    //dybde skjekk ikke null verdi
+    public int dybde_beskyttet(AVLnode node){
+        if (node == null) {
+            return -1;
+        } 
+        return node.dybde;
+    }
+
+
+
+    public boolean contains(int value) {
+        AVLnode iter = root;
+
+        while (iter != null) {
+            if(value == iter.data) {return true;}
+            if (iter.data < value) {
+                iter = iter.right;
+            }
+            else {
+                iter = iter.left;
+            }
+        }
+        return false; 
+    }
+
+    public int size() {
+        return counter;
+    }
+
     public int getDybde(int value){
         AVLnode iter = root;
 
@@ -29,481 +146,6 @@ public class AVL_tree {
         
     }
 
-    
-
-    public int size() {
-        return counter;
-    }
-
-    public boolean contains(int value) {
-        AVLnode iter = root;
-
-        while (iter != null) {
-            if(value == iter.data) {return true;}
-            if (iter.data < value) {
-                iter = iter.right;
-            }
-            else {
-                iter = iter.left;
-            }
-        }
-        return false; 
-    }
-
-    //itterativ
-    public void insert1(int value){
-        AVLnode iter = root;
-        AVLnode previous;
-        AVLnode ny = new AVLnode(value);
-
-        if (root == null) {
-            root = ny; 
-            counter++; 
-            return;      
-        }
-        while (true) {
-            if(value == iter.data) {return;}
-            previous = iter;
-
-            if (iter.data < value) {
-                iter = iter.right;
-
-                if (iter == null) {
-                    previous.right = ny;
-                    ny.parent = previous;
-                    counter++;
-                    AVLnode current = ny;
-
-                    while (current.parent != null){
-                        current = current.parent;
-
-                        int leftH;
-                        if (current.left == null) {
-                            leftH = -1;
-                        } else {
-                            leftH= current.left.dybde;
-                        }  
-                        int rightH;
-                        if (current.right == null) {
-                            rightH = -1;
-                        } else {
-                            rightH = current.right.dybde;
-                        }
-                        current.dybde = 1 + Math.max(rightH, leftH);
-                        
-                    }
-
-                    return;
-                }
-            }
-            else {
-                iter = iter.left;
-
-                if (iter == null){
-                    previous.left = ny;
-                    ny.parent = previous;
-                    counter++;
-
-                    AVLnode current = ny;
-
-                    while (current.parent != null){
-                        current = current.parent;
-
-                        int leftH;
-                        if (current.left == null) {
-                            leftH = -1;
-                        } else {
-                            leftH= current.left.dybde;
-                        }  
-                        int rightH;
-                        if (current.right == null) {
-                            rightH = -1;
-                        } else {
-                            rightH = current.right.dybde;
-                        }
-                        current.dybde = 1 + Math.max(rightH, leftH);
-                        
-                    }
-
-                    return;
-                }
-            }
-        } 
-    }
-
-    
-    // insert kallet som kan kalle balanser etter insertRec
-    public void insert(int value) {
-        root = insertRec(root, value);
-        counter++;
-    }
-
-    // Rekursiv insert fra roten så vi slipper parent i rotate, mere pekere og flytte mere kode da.
-    private AVLnode insertRec(AVLnode node, int value) {
-        if (node == null) {
-            return new AVLnode(value); // ny node som blad
-        }
-
-        // rekursiv obdatering av node dybde
-        if (value < node.data) {
-            node.left = insertRec(node.left, value);
-        } else if (value > node.data) {
-            node.right = insertRec(node.right, value);
-        } else {
-            return node; // duplikat
-        }
-
-         // skjekker at de ikke er 0 i høyden
-        int leftH;
-        if (node.left == null) {
-            leftH = -1;
-        } else {
-            leftH= node.left.dybde;
-        }  
-        int rightH;
-        if (node.right == null) {
-            rightH = -1;
-        } else {
-            rightH = node.right.dybde;
-        }
-
-
-        node.dybde = 1 + Math.max(leftH, rightH);
-
-        return node;
-    }
-
-    public void remove(int value) {
-        AVLnode iter = root;
-        if (root.data == value) {
-            if (root.left == null && root.right == null) {
-                root = null;
-                counter--;
-                return;
-            }
-            if (root.right == null && root.left != null) {
-                root = root.left;
-                root.parent = null;
-                counter--;
-                return;
-            }
-            if (root.left == null && root.right != null) {
-                root = root.right;
-                root.parent = null;
-                counter--;
-                return;
-            }
-            else {
-                iter = root.right;
-                while (iter.left != null) {
-                    iter = iter.left;
-                }
-                root.data = iter.data;
-                iter.parent.left = null;
-                iter.parent = null;
-                counter--;
-                return;
-            }
-        }
-        while (iter != null) {
-            //sjekk om node er value
-            if (iter.data == value) {
-                //hvis noden har 0 barn
-                if (iter.left == null && iter.right == null) {
-                    //sjekk om den er på høyre eller venstre side
-                    if (iter == iter.parent.right) {
-                        iter.parent.right = null;
-                        counter--;
-                        return;
-                    }
-                    if (iter == iter.parent.left) {
-                        iter.parent.left = null;
-                        counter--;
-                        return;
-                    }
-                }
-
-                //hvis noden har 1 barn
-                if (iter.left == null && iter.right != null || iter.right == null && iter.left != null) {
-                    //sjekk hvilken side barnet er på
-                    //node.previous peker på node.barn
-                    if (iter == iter.parent.right) {
-                        if (iter.left == null) {
-                            iter.parent.right = iter.right;
-                            iter.right.parent = iter.parent;
-                            iter.parent = null;
-                            iter.right = null;
-                            counter--;
-                            return;
-                        }
-                        if (iter.right == null) {
-                            iter.parent.right = iter.left;
-                            iter.left.parent = iter.parent;
-                            iter.parent = null;
-                            iter.left = null;
-                            counter--;
-                            return;
-                        }
-                    }
-                    if (iter == iter.parent.left) {
-                        if (iter.left == null) {
-                            iter.parent.left = iter.right;
-                            iter.right.parent = iter.parent;
-                            iter.parent = null;
-                            iter.right = null;
-                            counter--;
-                            return;
-                        }
-                        if (iter.right == null) {
-                            iter.parent.left = iter.left;
-                            iter.left.parent = iter.parent;
-                            iter.parent = null;
-                            iter.left = null;
-                            counter--;
-                            return;
-                        }
-                    }
-                }
-
-                //hvis noden har 2 barn
-                    if (iter.right != null && iter.left != null) {
-                        //gå til den mest venstre verdien på høyre subtre, sett denne noden til iter.value, iter.previous.right/left 
-                        //(må sjekke hvilken side den er på), må oppdate referanser til den som blir flyttet også
-                        AVLnode iter2 = iter.right;
-                        while (iter2.left != null) {
-                            iter2 = iter2.left;
-                        }
-
-                        if (iter2.right != null) {
-                            iter.data = iter2.data;
-                            iter2.parent.left = iter2.left;
-                            iter2.right.parent = iter2.parent;
-                            iter2.parent.left = null;
-                            iter2.parent = null;
-                            counter--;
-                            return;
-                        }
-
-                        iter.data = iter2.data;
-                        iter2.parent.left = null;
-                        iter2.parent = null;
-                        counter--;
-                        return;
-                    }        
-            }
-
-            //er node større eller mindre enn iter
-            //hvis større, gå høyre
-            if (iter.data < value) {
-                iter = iter.right;
-                System.out.println("går høyre");
-            }
-            //hvis mindre (ellers), gå venstre
-            else {
-                iter = iter.left;
-                System.out.println("går venstre");
-            }
-        }        
-    }
-
-    public void remove2(int value){
-        root = removeRec(root, value);
-        counter--;
-
-    }
-    
-    
-    private AVLnode removeRec(AVLnode node, int value){
-
-        if (node == null){
-
-        }
-    
-            // rekursiv obdatering av node dybde
-    
-        if (value < node.data) {
-    
-            node.left = insertRec(node.left, value);
-    
-        } else if (value > node.data) {
-    
-            node.right = insertRec(node.right, value);
-    
-        } else {
-    
-            return node; // duplikat
-    
-        }
-
-
-    
-        // skjekker at de ikke er 0 i høyden
-    
-        int leftH;
-    
-        if (node.left == null) {
-    
-            leftH = -1;
-    
-        } else {
-    
-            leftH= node.left.dybde;
-    
-        }  
-    
-        int rightH;
-    
-        if (node.right == null) {
-    
-            rightH = -1;
-    
-        } else {
-    
-            rightH = node.right.dybde;
-    
-        }
-
-
-        node.dybde = 1 + Math.max(leftH, rightH);
-
-
-    
-        return node;
-
-
-    
-    }
-
-    // har ikke fikset dybde vet begynner tro rekusjon er bedre.
-    public void remove1(int value) {
-        AVLnode iter = root;
-        if (root.data == value) {
-            if (root.left == null && root.right == null) {
-                root = null;
-                counter--;
-                return;
-            }
-            if (root.right == null && root.left != null) {
-                root = root.left;
-                counter--;
-                return;
-            }
-            if (root.left == null && root.right != null) {
-                root = root.right;
-                counter--;
-                return;
-            }
-            else {
-                iter = root.right;
-                while (iter.left != null) {
-                    iter = iter.left;
-                }
-                root.data = iter.data;
-                counter--;
-                return;
-            }
-        }
-        AVLnode forrige = root;
-        while (iter != null) {
-
-            //sjekk om node er value
-            if (iter.data == value) {
-                //hvis noden har 0 barn
-                if (iter.left == null && iter.right == null) {
-                    //sjekk om den er på høyre eller venstre side
-                    if (iter == forrige.right) {
-                        forrige.right = null;
-                        counter--;
-                        return;
-                    }
-                    if (iter == forrige.left) {
-                        forrige.left = null;
-                        counter--;
-                        return;
-                    }
-                }
-
-                //hvis noden har 1 barn
-                if (iter.left == null && iter.right != null || iter.right == null && iter.left != null) {
-                    //sjekk hvilken side barnet er på
-                    //node.previous peker på node.barn
-                    if (iter == forrige.right) {
-                        if (iter.left == null) {
-                            forrige.right = iter.right;
-                            counter--;
-                            return;
-                        }
-                        if (iter.right == null) {
-                            forrige.right = iter.left;
-                           
-                            counter--;
-                            return;
-                        }
-                    }
-                    if (iter == forrige.left) {
-                        if (iter.left == null) {
-                            forrige.left = iter.right;
-                            counter--;
-                            return;
-                        }
-                        if (iter.right == null) {
-                            forrige.left = iter.left;
-                            counter--;
-                            return;
-                        }
-                
-                    }
-
-                    
-
-                }
-
-                //hvis noden har 2 barn
-                    if (iter.right != null && iter.left != null) {
-                        //gå til den mest venstre verdien på høyre subtre, sett denne noden til iter.value, iter.previous.right/left 
-                        //(må sjekke hvilken side den er på), må oppdate referanser til den som blir flyttet også
-                        AVLnode forrige2 = iter;
-                        AVLnode iter2 = iter.right;
-                        while (iter2.left != null) {
-                            forrige2 = iter2;
-                            iter2 = iter2.left;
-                        }
-
-
-                        if (iter2.right != null) {
-                            iter.data = iter2.data;
-                            forrige2.left = null;
-                            forrige2.left = iter2.right;
-                            counter--;
-                            return;
-                        }
-
-                        iter.data = iter2.data;
-                        forrige2.left = null;
-                        
-                        counter--;
-                        return;
-                    }        
-            }
-
-            //er node større eller mindre enn iter
-            //hvis større, gå høyre
-            if (iter.data < value) {
-                forrige = iter;
-                iter = iter.right;
-                System.out.println("går høyre");
-            }
-            //hvis mindre (ellers), gå venstre
-            else {
-                forrige = iter;
-                iter = iter.left;
-                System.out.println("går venstre");
-            }
-        }        
-    }
-
-   
-
 
 
     public static void main(String[] args) {
@@ -523,7 +165,7 @@ public class AVL_tree {
 
             switch (instruksjon) {
                 case "insert":
-                    tree.insert1(verdi);
+                    tree.insert(verdi);
                     break;
                 case "contains":
                     System.out.println(tree.contains(verdi));
--- AVLnode.java
+++ AVLnode.java
@@ -1,12 +1,13 @@
 public class AVLnode {
    int data;
+   int dybde;
    AVLnode right = null;
    AVLnode left = null;
-   int dybde;
    AVLnode parent = null;
 
    public AVLnode(int data) {
       this.data = data;
       dybde = 0;
    }
+   
 }
