4bbc96592ae502980acd59969a7e45766a5b841a
isakfelder <isakfelder@gmail.com>
2025-09-23 17:14:05 +0200

    commit

--- Oblig2/Sorter.java
+++ Oblig2/Sorter.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+
 public class Sorter {
-    
+    File input;
+    public static void main(String[] args) {
+        
+    }
 }
